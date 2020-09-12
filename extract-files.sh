ba5d17bc4fa01f03eab27227713559699e589fa6
Arian <arian.kulmer@web.de>
2020-09-12 13:00:40 +0200

    sdm660-common: Patchelf libhwbinder and libhidltransport dependencies from proprietary files away
    Change-Id: Idf08428159edfdead9fb5bbe7ad9200fbed9e60e
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -96,3 +96,13 @@ if [ -s "$MY_DIR"/../$DEVICE/proprietary-files.txt ]; then
 fi
 
 "$MY_DIR"/setup-makefiles.sh
+
+for i in $(grep -rn 'libhidltransport.so\|libhwbinder.so' ../../../vendor/xiaomi/"${DEVICE_SPECIFIED_COMMON}"/proprietary | awk '{print $3}'); do
+	patchelf --remove-needed "libhwbinder.so" "$i"
+	patchelf --remove-needed "libhidltransport.so" "$i"
+done
+
+for i in $(grep -rn 'libhidltransport.so\|libhwbinder.so' ../../../vendor/xiaomi/"${DEVICE_COMMON}"/proprietary | awk '{print $3}'); do
+	patchelf --remove-needed "libhwbinder.so" "$i"
+	patchelf --remove-needed "libhidltransport.so" "$i"
+done
