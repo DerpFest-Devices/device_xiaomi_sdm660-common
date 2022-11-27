a057f3e76c5b015a4900bd8b05b53775b043d649
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-11-27 16:44:51 +0100

    sdm660-common: Remove TARGET_ENABLE_MEDIADRM_64
    * It's only used when building drm@1.0 HIDL
    
    Change-Id: I06412e2dd2bb48d798be4ef6e9a26a17b65277a7

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -74,9 +74,6 @@ TARGET_USES_ION := true
 
 OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 
-# DRM
-TARGET_ENABLE_MEDIADRM_64 := true
-
 # DT2W
 TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
 
