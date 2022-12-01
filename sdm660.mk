6af29e3745f86ba586b4ba7e5efb78e26a903a49
NurKeinNeid <mralexman3000@gmail.com>
2022-12-01 17:14:03 +0100

    sdm660-common: Build libmiwatermark_shim
    Change-Id: I24d093638eb6b8ff1d4ad218f7cc2f303711766f
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -382,6 +382,9 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl:64 \
     android.hardware.sensors@1.0-service
 
+PRODUCT_PACKAGES += \
+    libmiwatermark_shim
+
 # Tetheroffload
 PRODUCT_PACKAGES += \
     ipacm \
