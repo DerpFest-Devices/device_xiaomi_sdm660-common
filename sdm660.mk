8824c02c45a74b1cb431a1ae9004a30bd43aa22e
NurKeinNeid <mralexman3000@gmail.com>
2023-05-18 16:44:59 +0200

    Revert "sdm660-common: Drop non-functional soundtrigger"
    This reverts commit 3abde98bcb1fb6ce3c79dec9e0a3216f4877e2e5.

--- sdm660.mk
+++ sdm660.mk
@@ -64,6 +64,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl:32 \
     android.hardware.audio.effect@7.0-impl:32 \
     android.hardware.audio.service \
+    android.hardware.soundtrigger@2.1-impl:32 \
     audio.primary.sdm660 \
     audio.r_submix.default \
     audio.usb.default \
